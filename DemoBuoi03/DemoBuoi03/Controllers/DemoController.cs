868b4e8015bb401512af99902d122547c9aee79a
HIENLTH 2023 <hyhien@gmail.com>
2024-06-28 13:39:41 +0700

    Demo sync

---
+++ DemoBuoi03/DemoBuoi03/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using DemoBuoi03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoBuoi03.Controllers
+{
+	public class DemoController : Controller
+	{
+		public IActionResult SyncDemo()
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			MyClass.FuncA();
+			MyClass.FuncB();
+			MyClass.FuncC();
+			sw.Stop();
+
+			return Content($"Chạy sync hết {sw.ElapsedMilliseconds}ms");
+		}
+	}
+}
---
+++ DemoBuoi03/DemoBuoi03/Models/MyClass.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoBuoi03.Models
+{
+	public class MyClass
+	{
+		public static int FuncA()
+		{
+			Thread.Sleep(2000);
+			return 0;
+		}
+		public static string FuncB()
+		{
+			Thread.Sleep(5000);
+			return "21BITV03";
+		}
+		public static void FuncC()
+		{
+			Thread.Sleep(3000);
+		}
+	}
+}
