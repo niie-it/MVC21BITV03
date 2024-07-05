6db3476a2251667f336341fb642ad923ebf6699c
HIENLTH 2023 <hyhien@gmail.com>
2024-07-05 16:34:35 +0700

    Demo basic validation attribute

---
+++ DemoValidation/DemoValidation/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using DemoValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoValidation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Employee emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("loi", "Còn lỗi k");
+            }
+            return View();
+        }
+    }
+}
--- DemoValidation/DemoValidation/Models/Employee.cs
+++ DemoValidation/DemoValidation/Models/Employee.cs
@@ -5,24 +5,39 @@ namespace DemoValidation.Models
     public class Employee
     {
         public int? Id { get; set; }
+
+        [RegularExpression(@"[a-z]{6}", ErrorMessage ="Đúng 6 kí tự")]
         public string EmployeeNo { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Từ 3 đến 100 kí tự")]
         public string FullName { get; set; }
+
+
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
+
+
         public DateTime BirthDate { get; set; }
+
         public string? Email { get; set; }
+
+        [RegularExpression(@"0[98753]\d{8}", ErrorMessage ="Số di động gồm 10 chữ số")]
         public string Phone { get; set; }
+
         public string? Address { get; set; }
         public bool Gender { get; set; }
         public string? Website { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage ="Lương phải không âm")]
         public double Salary { get; set; }
         public bool IsPartTime { get; set; }
-        public string? CreditCard{ get; set; }
+        public string? CreditCard { get; set; }
 
         [DataType(DataType.MultilineText)]
-        public string? Description{ get; set; }
+        public string? Description { get; set; }
     }
 }
