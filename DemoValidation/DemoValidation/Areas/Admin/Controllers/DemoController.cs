fb2434db8f4ee265cedf79b10699aed0e36bc744
HIENLTH 2023 <hyhien@gmail.com>
2024-07-19 16:31:30 +0700

    Demo Layout, Partial View, Area

---
+++ DemoValidation/DemoValidation/Areas/Admin/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoValidation.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class DemoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
