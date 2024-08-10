9c0e00fe3a5d5ece22d5ac1c4cf02ae28d3dae14
HIENLTH 2023 <hyhien@gmail.com>
2024-08-10 07:19:28 +0700

    Lab07

---
+++ Lab07/Lab07/Controllers/SuppliersController.cs
@@ -0,0 +1,38 @@
+﻿using Lab07.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab07.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly MvcNiieLabContext _ctx;
+
+        public SuppliersController(MvcNiieLabContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_ctx.Suppliers.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Supplier model, IFormFile LogoImage)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Add(model);
+                _ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
